ae37bdddc411ac102e2caf23f4574b27fc2d1c90
Ron Gerlach <wordsmith4u@hotmail.com>
2020-09-26 15:49:31 -0500

    Revised

--- ch08_proj02_PersonManager/src/PersonManagerApp.java
+++ ch08_proj02_PersonManager/src/PersonManagerApp.java
@@ -8,27 +8,30 @@ public class PersonManagerApp {
 
 		Person p = new Person();
 
-		Scanner sc = new Scanner(System.in);
-		String choice = "y";
+		Scanner sc = new Scanner(System.in); // Set scanner
+		String choice = "y"; // as long as we keep getting input, we keep scanning
 		while (choice.equalsIgnoreCase("y")) {
-			System.out.print("Create customer or employer? (c/e): ");
-			String creation = sc.next();
+
+			System.out.print("Create customer or employer? (c/e): "); // prompt for c or e
+			String creation = sc.next(); // user answer
 			sc.nextLine();
-			String fn = Console.getString("Enter first name:  ");
-			String ln = Console.getString("Enter last name: ");
+			String fn = Console.getString("Enter first name:  "); // prompt for first name
+			String ln = Console.getString("Enter last name: "); // prompt for last name
 			System.out.println();
 
+			// verify if the customer is "c" or "e"
 			if (creation.equalsIgnoreCase("c")) {
-				Customer c = new Customer();
-				String cNumber = Console.getString("Customer Number:     ");
+				Customer c = new Customer(); // declare new customer
+				String cNumber = Console.getString("Customer Number:     "); // prompt for customer number
 				c.setCustomerNumber(cNumber);
-				c.setFirstName(fn);
-				c.setLastName(ln);
-				System.out.println("You entered a new customer" + "\n" + c.toString());
+				c.setFirstName(fn); // call first name
+				c.setLastName(ln); // call last name
+				System.out.println("You entered a new customer" + "\n" + c.toString()); // print out with Override
+																						// string
 				System.out.println();
 			} else if (creation.equalsIgnoreCase("e")) {
 				Employer e = new Employer();
-				String social = Console.getString("SSN:     ");
+				String social = Console.getString("SSN:     "); // prompt for SSN
 				e.setFirstName(fn);
 				e.setLastName(ln);
 				e.setSsn(social);
@@ -39,17 +42,16 @@ public class PersonManagerApp {
 				System.out.println("This entry is required. Try again");
 			}
 
-			System.out.print("Continue? (y/n): ");
+			// Ask user if they want to continue
+			choice = Console.getString("Continue? (y/n): ");
 			if (choice.equalsIgnoreCase("y")) {
 				choice = sc.nextLine();
 			} else if (choice.equalsIgnoreCase("n")) {
 				System.out.println("Goodbye");
 			} else {
 				System.out.println("Error! Entry must be 'y' or 'n'. Try again.");
-				continue;
 			}
 			System.out.println();
-
 		}
 	}
 }
