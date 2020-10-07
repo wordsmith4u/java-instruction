ce2f74209cd29a4fc50032f0ad72c25966aa6318
Ron Gerlach <wordsmith4u@hotmail.com>
2020-10-06 19:36:35 -0500

    new wizard inventory

--- ch12_Proj02_WizardInventory/src/WizardInventoryApp.java
+++ ch12_Proj02_WizardInventory/src/WizardInventoryApp.java
@@ -1,5 +1,6 @@
 
-import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
 
 public class WizardInventoryApp {
 
@@ -7,58 +8,94 @@ public class WizardInventoryApp {
 
 	public static void main(String[] args) {
 
-		System.out.println("The Wizard Inventory Game");
-		System.out.println();
-		System.out.println("COMMAND MENU");
-		System.out.println("show - Show all items\n" + "grab - Grab and item\n" + "edit - Edit an item\n"
-				+ "drop - Drop an item\n" + "exit - Exit program");
-		System.out.println();
+		printWelcomeMessage();
 
-		ArrayList<String> menu = new ArrayList<>();
-		menu.add("wooden staff");
-		menu.add("wizard hat");
-		menu.add("cloth shoes");
+		List<String> inventory = new LinkedList<>();
+		inventory.add("hat");
+		inventory.add("wand");
+		inventory.add("cat");
 
-		String choice = "y";
-		while (choice.equalsIgnoreCase("y")) {
-			String commandCode = Console.getString("Command: ");
-			if (commandCode.equalsIgnoreCase("show")) {
-				for (String s : menu) {
-					System.out.println(s);
-				}
-			} else if (commandCode.equalsIgnoreCase("drop")) {
-				int code = Console.getInt("Number: ");
-				String codeRemove = menu.remove(code - 1);
-				System.out.println(codeRemove + " " + "was dropped.");
+		String command = "go";
+		while (!command.equalsIgnoreCase("exit")) {
 
-			} else if (commandCode.equalsIgnoreCase("grab")) {
-				String newItem = Console.getString("Name: ");
-				menu.add(newItem);
-				System.out.println(newItem + " " + "was added.");
+			System.out.println();
+			command = Console.getString("Command: ");
 
-			} else if (menu.size() >= MAX_INVENTORY_SIZE) {
-				System.out.println("You can't carry anymore items. Drop something first");
-				return;
+			switch (command.toLowerCase()) {
+			case "show":
+				showInventory(inventory);
+				break;
 
-			} else if (commandCode.equalsIgnoreCase("edit")) {
-				int oneBasedIndex = Console.getInt("Number: ");
-				int zeroBasedIndex = oneBasedIndex - 1;
+			case "grab":
+				addItemToInventory(inventory);
+				break;
 
-				if (zeroBasedIndex > 0 || zeroBasedIndex >= menu.size()) {
-					System.out.println("Index Invalid");
-					return;
-				}
+			case "edit":
+				editInventory(inventory);
+				break;
 
-				String newItemName = Console.getString("Updated name: ");
-				menu.set(zeroBasedIndex, newItemName);
-				System.out.println(newItemName);
-			}
+			case "drop":
+				dropItemFromInventory(inventory);
+				break;
 
-			else {
-				System.out.println("Bye");
+			case "exit":
+				System.out.println("Bye!");
+				break;
+
+			default:
+				System.out.println("Invalid command");
 				break;
 			}
-			System.out.println();
 		}
+	}
+
+	private static void printWelcomeMessage() {
+		System.out.println("The Wizard Inventory Game");
+		System.out.println("COMMAND MENU\r\n" + "show - Show all items\r\n" + "grab - Grab an item\r\n"
+				+ "edit - Edit an item\r\n" + "drop - Drop an item\r\n" + "exit - Exit program\r\n");
+	}
+
+	private static void showInventory(List<String> inventory) {
+		for (String item : inventory) {
+			System.out.println((inventory.indexOf(item) + 1) + ". " + item);
+		}
+	}
+
+	private static void addItemToInventory(List<String> inventory) {
+		if (inventory.size() >= MAX_INVENTORY_SIZE) {
+			System.out.println("You can't carry any more items. Drop something first");
+			return;
+		}
+
+		String newItem = Console.getString("Name: ");
+		inventory.add(newItem);
+		System.out.println(newItem + " was added");
+	}
+
+	private static void editInventory(List<String> inventory) {
+		int oneBasedIndex = Console.getInt("Number: ");
+		int zeroBasedIndex = oneBasedIndex - 1;
+
+		if (zeroBasedIndex < 0 || zeroBasedIndex >= inventory.size()) {
+			System.out.println("Index invalid");
+			return;
+		}
+
+		String newItemName = Console.getString("Updated name: ");
+		String previousItemName = inventory.set(zeroBasedIndex, newItemName);
+		System.out.println("Replaced " + previousItemName + " with " + newItemName);
+	}
+
+	private static void dropItemFromInventory(List<String> inventory) {
+		int oneBasedIndex = Console.getInt("Number: ");
+		int zeroBasedIndex = oneBasedIndex - 1;
+
+		if (zeroBasedIndex < 0 || zeroBasedIndex >= inventory.size()) {
+			System.out.println("Index invalid");
+			return;
+		}
+
+		String removedItem = inventory.remove(zeroBasedIndex);
+		System.out.println(removedItem + " was removed");
 	}
 }
