ec8f0f799cca7cd0860f5f636993c125a94c6a65
Ron Gerlach <wordsmith4u@hotmail.com>
2020-12-31 11:36:25 -0500

    new commits

---
+++ Make_Chocolate/src/MakeChocolateApp.java
@@ -0,0 +1,23 @@
+public class MakeChocolateApp {
+
+	public static void main(String[] args) {
+
+	}
+
+	public int makeChocolate(int small, int big, int goal) {
+
+		int rem = goal % 5;
+
+		if (small + (big * 5) < goal)
+			return -1;
+
+		else if (rem <= small && goal - big * 5 > 4)
+			return rem + 5;
+
+		else if (rem <= small)
+			return rem;
+
+		else
+			return -1;
+	}
+}
